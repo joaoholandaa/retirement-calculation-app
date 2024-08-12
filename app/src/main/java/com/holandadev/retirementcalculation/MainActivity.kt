f544fa099c0deaad9adaf49d1dbe0aba2d000679
Joao Holanda <joaocmdt505@gmail.com>
2024-08-12 14:12:05 -0300

    feat: Create MainActivity

---
+++ app/src/main/java/com/holandadev/retirementcalculation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.holandadev.retirementcalculation
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity() : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
